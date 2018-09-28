055a04a0e6ac52d9c6a0c935c8b3ab2786b4bc81
ariekzn <32986135+ariekzn@users.noreply.github.com>
2018-09-28 17:02:02 +0700

    Update wa-io.js

--- wa-io.js
+++ wa-io.js
@@ -1,18 +1,18 @@
-       (function () {
+    (function () {
         var options = {
-            whatsapp: &quot;+6281219220752&quot;, // WhatsApp number
-            email: &quot;office@suryareadymix.com&quot;, // Email
-            sms: &quot;+6281219220752&quot;, // Sms phone number
-            call: &quot;+6281219220752&quot;, // Call phone number
-            company_logo_url: &quot;//2.bp.blogspot.com/-q8oRFlhQeo4/WmiWt5Q9SGI/AAAAAAAAAd4/Dw1Zjvc7vdwLA1THD2cLSygUO3fFGdc7ACLcBGAs/s320/logo%2Bp.png&quot;, // URL of company logo (png, jpg, gif)
-            greeting_message: &quot;Hallo, ada yang bisa kami bantu? Silahkan tinggalkan pesan sekarang.&quot;, // Text of greeting message
-            call_to_action: &quot;Hubungi Kami&quot;, // Call to action
-            button_color: &quot;#129BF4&quot;, // Color of button
-            position: &quot;right&quot;, // Position may be &#39;right&#39; or &#39;left&#39;
-            order: &quot;whatsapp,call,sms,email&quot; // Order of buttons
+            whatsapp: "+6287741114299", // WhatsApp number
+            email: "office@muliareadymix.com", // Email
+            sms: "+6287741114299", // Sms phone number
+            call: "+6282122929433", // Call phone number
+            company_logo_url: "//2.bp.blogspot.com/-q8oRFlhQeo4/WmiWt5Q9SGI/AAAAAAAAAd4/Dw1Zjvc7vdwLA1THD2cLSygUO3fFGdc7ACLcBGAs/s320/logo%2Bp.png", // URL of company logo (png, jpg, gif)
+            greeting_message: "Hallo, ada yang bisa kami bantu? Silahkan tinggalkan pesan sekarang.", // Text of greeting message
+            call_to_action: "Hubungi Kami", // Call to action
+            button_color: "#129BF4", // Color of button
+            position: "right", // Position may be 'right' or 'left'
+            order: "whatsapp,call,sms,email" // Order of buttons
         };
-        var proto = document.location.protocol, host = &quot;whatshelp.io&quot;, url = proto + &quot;//static.&quot; + host;
-        var s = document.createElement(&#39;script&#39;); s.type = &#39;text/javascript&#39;; s.async = true; s.src = url + &#39;/widget-send-button/js/init.js&#39;;
+        var proto = document.location.protocol, host = "whatshelp.io", url = proto + "//static." + host;
+        var s = document.createElement('script'); s.type = 'text/javascript'; s.async = true; s.src = url + '/widget-send-button/js/init.js';
         s.onload = function () { WhWidgetSendButton.init(host, proto, options); };
-        var x = document.getElementsByTagName(&#39;script&#39;)[0]; x.parentNode.insertBefore(s, x);
+        var x = document.getElementsByTagName('script')[0]; x.parentNode.insertBefore(s, x);
     })();
